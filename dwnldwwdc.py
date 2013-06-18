053d3957c69d55134fb1f6e80b80d4cf9a5286c5
jicea <jc@manbolo.com>
2013-06-18 14:58:12 +0200

    First version

---
+++ dwnldwwdc.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import sys
+import os
+import urllib
+import argparse
+from bs4 import BeautifulSoup
+
+
+def report(count, block_size, total_size):
+    """Report save progression."""
+    progress_size = int(count * block_size) / (1024 * 1024)
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r  {0}%, {1} MB".format(percent, progress_size))
+    sys.stdout.flush()
+
+
+def save(url, dst, force=False):
+    """Download a file at url to local folder."""
+    if not os.path.isfile(dst) or force:
+        # Test if the directory exist or create
+        d = os.path.dirname(dst)
+        if not os.path.exists(d):
+            os.makedirs(d)
+        print(u"\nDownloading: {0} to {1}".format(url, dst))
+        urllib.urlretrieve(url, dst, report)
+
+
+def download(folder_dst, pdf, sd, hd):
+    """Parse the WWDC html page at https://developer.apple.com/wwdc/videos/.
+    The page has been downloaded locally to prevent signin etc... Not the
+    better solution, bu a pragmatic one"""
+    html_src = os.path.abspath(os.path.join(os.path.dirname(__file__), 'wwdc2013.html'))
+
+    try:
+        soup = BeautifulSoup(open(html_src, 'r').read())
+    except IOError:
+        print("No wwdc2013.html here! Download the file from: ")
+        return -1
+
+    for div in soup.find_all('div', 'description active'):
+        ul = div.find('ul')
+        li = ul.find_all('li')
+
+        title = unicode(li[0].text)
+        session = unicode(li[1].text)
+        p = div.find('p', 'download')
+        if not p:
+            continue
+        a = p.find_all('a')
+
+        if len(a) and hd:
+            url = a[0].get('href')
+            dst = u"{0}/Videos/{1} - {2} - HD.mov".format(folder_dst, title, session)
+            save(url, dst)
+
+        if len(a) > 1 and sd:
+            url = a[1].get('href')
+            dst = u"{0}/Videos/{1} - {2}.mov".format(folder_dst, title, session)
+            save(url, dst)
+
+        if len(a) > 2 and pdf:
+            url = a[2].get('href')
+            dst = u"{0}/Slides/{1} - {2}.pdf".format(folder_dst, title, session)
+            save(url, dst)
+
+    print(u"\nDone!")
+    return 0
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dst", help="destination folder")
+    parser.add_argument("--pdf", help="download only PDF", action="store_true", default=False)
+    parser.add_argument("--sd", help="download only SD videos", action="store_true", default=False)
+    parser.add_argument("--hd", help="download only HD videos", action="store_true", default=False)
+    args = parser.parse_args()
+
+    sys.exit(download(folder_dst=args.dst, pdf=args.pdf, sd=args.sd, hd=args.hd))
+
+
